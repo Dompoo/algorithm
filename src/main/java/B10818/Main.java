26d597b88703eecf69e5f416efb6b711255d4eae
Dompoo <dlckdrms0517@gmail.com>
2024-01-16 21:08:22 +0900

    10818 문제 해결

---
+++ src/main/java/B10818/Main.java
@@ -0,0 +1,20 @@
+package B10818;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arrSize = sc.nextInt();
+        int[] arr = new int[arrSize];
+
+        for (int i = 0; i < arrSize; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[0] + " " + arr[arrSize - 1]);
+    }
+}
