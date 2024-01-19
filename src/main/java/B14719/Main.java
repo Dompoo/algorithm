bd0d978683f7fa1488910f74906b5f1bf9b0a955
Dompoo <dlckdrms0517@gmail.com>
2024-01-20 00:32:55 +0900

    14719 문제 해결

--- src/main/java/B14719/Main.java
+++ src/main/java/B14719/Main.java
@@ -1,40 +1,48 @@
 package B14719;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    static int[] block;
+    static int[] rainArr;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int height = sc.nextInt();
         int width = sc.nextInt();
 
-        boolean[] isUp = new boolean[width];
-        int[] block = new int[width];
+        block = new int[width];
+        rainArr = new int[width];
 
         for (int i = 0; i < width; i++) {
             block[i] = sc.nextInt();
-            if (i == 0) {
-                isUp[i] = true;
-            } else {
-                isUp[i] = block[i - 1] <= block[i];
+        }
+
+        //모든 i < j 쌍에 대해서 물 높이를 구한다.
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < width; j++) {
+                if (i < j) {
+                    getRain(i, j);
+                }
             }
         }
-        /*
-        첫번째 블록은 무조건 +경향
-        나머지는 전 블록 보다 크거나 같으면 +
-        작으면 -
-        + - - + 같은 경향에서
-        + 중에 더 작은 블록의 높이 X - 의 개수 : 빈 공간의 크기
-        여기에 - 블록의 높이를 모두 빼면 빗물의 높이
-        */
 
-        int towerHeight = 0;
+        System.out.println(Arrays.stream(rainArr).sum());
+    }
 
-        for (int i = 0; i < width; i++) {
-            if (isUp[i]) {
-                towerHeight = block[i];
-            }
+    public static void getRain(int index1, int index2) {
+
+        int maxRain = Math.min(block[index1], block[index2]);
+        int rain;
+
+        //i와 j 사이의 블록에 대하여 물 높이를 구한다.
+        //모든 index1 과 index2에 대해서 함수가 작동하므로.
+        //결국 최대 물 높이가 나온다.
+        for (int i = index1 + 1; i < index2; i++) {
+            rain = maxRain - block[i];
+            rainArr[i] = Math.max(rainArr[i], rain);
         }
     }
 }
