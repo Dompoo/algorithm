c3932e256c437ad44586ebfa536f2e1a8f694df3
Dompoo <dlckdrms0517@gmail.com>
2024-02-09 17:00:04 +0900

    2748 문제 해결 - DP

---
+++ src/main/java/B2748/Main.java
@@ -0,0 +1,20 @@
+package B2748;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        long[] dp = new long[91];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= 90; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        System.out.println(dp[T]);
+    }
+}
