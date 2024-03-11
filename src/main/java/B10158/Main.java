f533549a658862483b84bb259790b36f4c2a8035
Dompoo <dlckdrms0517@gmail.com>
2024-03-11 14:33:20 +0900

    10158 문제 해결 - String, 최적화

--- src/main/java/B10158/Main.java
+++ src/main/java/B10158/Main.java
@@ -9,10 +9,13 @@ public class Main {
     static int maxX;
     static int maxY;
 
-    static int curX;
-    static int curY;
+    static int X;
+    static int Y;
 
     static int time;
+
+    static int ansX;
+    static int ansY;
 
     public static void input() throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -21,35 +24,27 @@ public class Main {
         maxY = Integer.parseInt(s.nextToken());
 
         s = new StringTokenizer(in.readLine()," ");
-        curX = Integer.parseInt(s.nextToken());
-        curY = Integer.parseInt(s.nextToken());
+        X = Integer.parseInt(s.nextToken());
+        Y = Integer.parseInt(s.nextToken());
 
         s = new StringTokenizer(in.readLine()," ");
         time = Integer.parseInt(s.nextToken());
     }
 
     public static void solve() {
-        int timeX = time % (maxX * 2);
-        int dx = 1;
-        for (int i = 0; i < timeX; i++) {
-            if (curX + dx > maxX || curX + dx < 0) {
-                dx = -dx;
-            }
-            curX += dx;
+        ansX = (X + time) % (maxX * 2);
+        if (ansX > maxX) {
+            ansX = (maxX * 2) - ansX;
         }
 
-        int timeY = time % (maxY * 2);
-        int dy = 1;
-        for (int i = 0; i < timeY; i++) {
-            if (curY + dy > maxY || curY + dy < 0) {
-                dy = -dy;
-            }
-            curY += dy;
+        ansY = (Y + time) % (maxY * 2);
+        if (ansY > maxY) {
+            ansY = (maxY * 2) - ansY;
         }
     }
 
     public static void output() {
-        System.out.printf("%d %d\n", curX, curY);
+        System.out.printf("%d %d\n", ansX, ansY);
     }
 
     public static void main(String[] args) throws IOException {
