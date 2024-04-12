bbf2c0d66a51daba84caf2220ce007a20f149f55
Dompoo <dlckdrms0517@gmail.com>
2024-04-13 02:26:25 +0900

    258712 문제 해결 - DP

---
+++ src/main/java/P258712/Soulution.java
@@ -0,0 +1,53 @@
+package P258712;
+
+public class Soulution {
+        public int solution(String[] friends, String[] gifts) {
+            int n = friends.length;
+
+            int[][] arr = new int[n][n];
+            int[] giftIdx = new int[n];
+
+            for(int i = 0; i < gifts.length; i++) {
+                String[] friend12 = gifts[i].split(" ");
+                int go = getFriendNum(friend12[0], friends);
+                int to = getFriendNum(friend12[1], friends);
+                arr[go][to]++;
+                giftIdx[go] += 1;
+                giftIdx[to] -= 1;
+            }
+
+            int[] giftNum = new int[n];
+            for(int i = 0; i < n; i++) {
+                for(int j = i + 1; j < n; j++) {
+                    if(arr[i][j] > arr[j][i]) {
+                        giftNum[i]++;
+                    } else if(arr[i][j] < arr[j][i]) {
+                        giftNum[j]++;
+                    } else {
+                        if(giftIdx[i] > giftIdx[j]) {
+                            giftNum[i]++;
+                        } else if (giftIdx[i] < giftIdx[j]) {
+                            giftNum[j]++;
+                        }
+                    }
+                }
+            }
+
+            int max = -1;
+            for(int i = 0; i < n; i++) {
+                if(giftNum[i] > max) {
+                    max = giftNum[i];
+                }
+            }
+            return max;
+        }
+
+        public int getFriendNum(String name, String[] frd) {
+            for(int i = 0; i < frd.length; i++) {
+                if(frd[i].equals(name)) {
+                    return i;
+                }
+            }
+            return 0;
+        }
+}
