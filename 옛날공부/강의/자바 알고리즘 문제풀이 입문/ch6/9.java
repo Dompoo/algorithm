56491f5f5a71057bcc38dc69291f2067a3923e10
Dompoo <dlckdrms0517@gmail.com>
2024-10-03 16:51:20 +0900

    자바 알고리즘 문제풀이 입문 : 문제 6-9

--- 옛날공부/강의/자바 알고리즘 문제풀이 입문/ch6/9.java
+++ 옛날공부/강의/자바 알고리즘 문제풀이 입문/ch6/9.java
@@ -1,17 +1,42 @@
+import java.util.*;
+
 class Main {
 
     public int solution(int size, int dvd, int[] arr) {
 
-        /*
-        9 3
-        1 2 3 4 5   15
-        6 7         13
-        8 9         17
+        int left = 0; //arr 중 최대값
+        int right = 0; //arr 전체 합
 
-        45 / 3 = 15
-        */
+        for(int i = 0; i < size; i++) {
+            left = Math.max(left, arr[i]);
+            right += arr[i];
+        }
 
-        return 100;
+        int result = 0;
+
+        while(left <= right) {
+            int mid = (left + right) / 2;
+
+            int count = 1;
+            int curr = 0;
+            for(int i = 0; i < size; i++) {
+                if(curr + arr[i] <= mid) {
+                    curr += arr[i];
+                } else {
+                    count++;
+                    curr = arr[i];
+                }
+            }
+
+            if(count <= dvd) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
