39a5ac0b865d8dc0b594b850659e741cef0440df
Dompoo <dlckdrms0517@gmail.com>
2024-10-03 17:58:33 +0900

    자바 알고리즘 문제풀이 입문 : 문제 7-8 (수학적 접근)

--- 옛날공부/강의/자바 알고리즘 문제풀이 입문/ch7/8.java
+++ 옛날공부/강의/자바 알고리즘 문제풀이 입문/ch7/8.java
@@ -72,10 +72,38 @@ class Main {
         return result;
     }
 
+    // 수학적 접근
+    public int solution2() {
+        int preAttemps = 0;
+        int preHyun = hyun;
+
+        if(hyun < song) {
+            // 작은 상태라면 5개씩 점프해서 가깝게 접근
+            while(song - preHyun >= 5) {
+                preHyun += 5;
+                preAttemps++;
+            }
+        } else if(hyun > song) {
+            // 큰 상태라면 -1로 그냥 return해버리기
+            return hyun - song;
+        }
+
+        int nextAttemps = 0;
+        switch(song - preHyun) {
+            case 4: nextAttemps = 2; break;
+            case 3: nextAttemps = 3; break;
+            case 2: nextAttemps = 2; break;
+            case 1: nextAttemps = 1; break;
+            case 0: nextAttemps = 0; break;
+        }
+
+        return preAttemps + nextAttemps;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         hyun = sc.nextInt();
         song = sc.nextInt();
-        System.out.println(new Main().solution());
+        System.out.println(new Main().solution2());
     }
 }
