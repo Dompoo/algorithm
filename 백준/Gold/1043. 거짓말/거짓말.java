d38e5e25ccc2924b6dad520b56540506e9fd70fa
이창근 <dlckdrms0517@gmail.com>
2024-10-15 20:47:06 +0900

    [Gold IV] Title: 거짓말, Time: 108 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -2,75 +2,92 @@ import java.util.*;
 import java.io.*;
 
 class Main {
+	static int[] parent;
+	static int find(int x) {
+		if (parent[x] == x) return x;
+		else return parent[x] = find(parent[x]);
+	}
+	
+	static void union(int a, int b) {
+		a = find(a);
+		b = find(b);
+		if (a != b) parent[b] = a;
+	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		String[] s = br.readLine().split(" ");
 		int peopleCount = Integer.parseInt(s[0]);
 		int partyCount = Integer.parseInt(s[1]);
 		
-		boolean[] truthArr = new boolean[peopleCount + 1];
-		Map<Integer, List<Integer>> partyMap = new HashMap<>();
+		parent = new int[peopleCount + 1];
+		for (int i = 1; i <= peopleCount; i++) {
+			parent[i] = i;
+		}
+		
+		List<List<Integer>> partyList = new ArrayList<>();
 		for (int p = 0; p < partyCount; p++) {
-			partyMap.put(p, new ArrayList<>());
+			partyList.add(new ArrayList<>());
 		}
 		
 		String[] truth = br.readLine().split(" ");
 		int truthSize = Integer.parseInt(truth[0]);
+		List<Integer> truthGroup = new ArrayList<>();
+		
 		for (int t = 1; t <= truthSize; t++) {
-			truthArr[Integer.parseInt(truth[t])] = true;
+			truthGroup.add(Integer.parseInt(truth[t]));
 		}
 		
 		for (int p = 0; p < partyCount; p++) {
 			String[] s2 = br.readLine().split(" ");
 			int size = Integer.parseInt(s2[0]);
 			for (int j = 1; j <= size; j++) {
-				partyMap.get(p).add(Integer.parseInt(s2[j]));
+				partyList.get(p).add(Integer.parseInt(s2[j]));
 			}
 		}
 		
-		int lastLieParty = partyMap.size();
-		while (true) {
-			
-			List<Integer> deletePartyList = new ArrayList<>();
-			
-			for (int keys : partyMap.keySet()) {
-				// 거짓말 할 수 있는 파티 업데이트
-				
-				for (int t = 1; t <= peopleCount; t++) {
-					
-					// 진실을 아는 사람이 파티에 포함되어 있다면
-					List<Integer> peoples = partyMap.get(keys);
-					if (truthArr[t] && peoples.contains(t)) {
-						// 해당 파티의 모든 사람이 진실을 아는 사람으로 분류됨
-						for (int people : peoples) {
-							truthArr[people] = true;
-						}
-						
-						// 해당 파티는 거짓말 할 수 있는 파티가 아님
-						deletePartyList.add(keys);
-						break;
+		// 파티정보로 union 연산 수행
+		for (List<Integer> party : partyList) {
+			int firstPerson = party.get(0);
+			for (int i = 1; i < party.size(); i++) {
+				union(firstPerson, party.get(i));
+			}
+		}
+		
+		// 진실을 아는 사람들의 그룹 확장
+		Set<Integer> expandedTruthGroup = new HashSet<>(truthGroup);
+		boolean changed;
+		do {
+			changed = false;
+			Set<Integer> newMembers = new HashSet<>();
+			for (int truthPerson : expandedTruthGroup) {
+				for (int i = 1; i <= peopleCount; i++) {
+					if (!expandedTruthGroup.contains(i) && find(truthPerson) == find(i)) {
+						newMembers.add(i);
+						changed = true;
 					}
 				}
 			}
-			
-//			System.out.println("party : " + partyMap);
-//			System.out.println("delete : " + deletePartyList);
-//			System.out.println();
-			
-			for (int delete : deletePartyList) {
-				partyMap.remove(delete);
+			expandedTruthGroup.addAll(newMembers);
+		} while (changed);
+		
+		truthGroup = new ArrayList<>(expandedTruthGroup);
+		
+		// 과장된 이야기를 할 수 있는 파티 계산
+		int result = 0;
+		for (List<Integer> party : partyList) {
+			boolean canLie = true;
+			for (int person : party) {
+				if (truthGroup.contains(find(person))) {
+					canLie = false;
+					break;
+				}
 			}
-			
-			
-			// 업데이트 후 변동이 없다면 종료
-			if (lastLieParty == partyMap.size()) {
-				break;
-			}
-			
-			lastLieParty = partyMap.size();
+			if (canLie) result++;
 		}
 		
-		System.out.print(lastLieParty);
+		
+		System.out.print(result);
 	}
 }
