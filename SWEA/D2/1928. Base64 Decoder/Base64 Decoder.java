b4e9e87bac6a5543a0dc7e142462bd6c96a8dab7
이창근 <dlckdrms0517@gmail.com>
2024-11-15 06:47:04 +0900

    [D2] Title: Base64 Decoder, Time: 114 ms, Memory: 18,388 KB -BaekjoonHub

--- SWEA/D2/1928. Base64 Decoder/Base64 Decoder.java
+++ SWEA/D2/1928. Base64 Decoder/Base64 Decoder.java
@@ -10,6 +10,19 @@ VG8gZG91YnQgaXMgc2FmZXIgdGhhbiB0byBiZSBzZWN1cmUu
 T25seSB0aGUganVzdCBtYW4gZW5qb3lzIHBlYWNlIG9mIG1pbmQu
 QSBmdWxsIGJlbGx5IGlzIHRoZSBtb3RoZXIgb2YgYWxsIGV2aWwu
  */
+/*
+인코딩 과정
+아스키 숫자로 바꿈
+8비트 이진수로 바꿈
+6비트씩 끊어서 10진수로 읽음 (0~63)
+컨버트
+ */
+/*
+디코딩 과정
+char 하나씩을 10진수로 컨버트
+10진수를 6비트로 컨버트 x N번
+8비트씩 끊어서 아스키로 읽음
+ */
 class Solution {
 	
 	public static void main(String[] args) throws Exception {
@@ -18,10 +31,39 @@ class Solution {
 		int testcase = Integer.parseInt(br.readLine().trim());
 		for (int t = 1; t <= testcase; t++) {
 			String input = br.readLine().trim();
-			byte[] decodedByte = Base64.getDecoder().decode(input);
-			String output = new String(decodedByte);
-			sb.append("#").append(t).append(" ").append(output).append("\n");
+			int buffer = 0;
+			int bitCount = 0;
+			sb.append("#").append(t).append(" ");
+			for(int i = 0; i < input.length(); i++) {
+				char ch = input.charAt(i);
+				int num = base64decode(ch);
+				buffer = (buffer << 6) | num;
+				bitCount += 6;
+				
+				while(bitCount >= 8) {
+					bitCount -= 8;
+					int charCode = (buffer >> bitCount) & 0xFF;
+					sb.append((char) charCode);
+				}
+			}
+			
+			sb.append("\n");
 		}
 		System.out.print(sb);
+	}
+	
+	private static int base64decode(char ch) {
+		if ('A' <= ch && ch <= 'Z') {
+			return ch - 'A';
+		} else if ('a' <= ch && ch <= 'z') {
+			return ch - 'a' + 26;
+		} else if ('0' <= ch && ch <= '9') {
+			return ch - '0' + 52;
+		} else if (ch == '+') {
+			return 62;
+		} else if (ch == '/') {
+			return 63;
+		}
+		throw new IllegalArgumentException();
 	}
 }
