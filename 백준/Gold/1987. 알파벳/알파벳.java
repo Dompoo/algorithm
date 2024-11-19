b86c779c6f4e10b789af04e0e06516996bc6878c
이창근 <dlckdrms0517@gmail.com>
2024-11-19 17:05:04 +0900

    [Gold IV] Title: 알파벳, Time: 840 ms, Memory: 15128 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,53 +1,52 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-class Main {
-
-    public static Character[][] arr;
-    public static int H;
-    public static int W;
-    public static int maxDistance = Integer.MIN_VALUE;
-    public static int[] dx = new int[]{0, 0, 1, -1};
-    public static int[] dy = new int[]{1, -1, 0, 0};
-
+/*
+2 4
+CAAB
+ADCB
+ */
+public class Main {
 	public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] s = br.readLine().split(" ");
-        H = Integer.parseInt(s[0]);
-        W = Integer.parseInt(s[1]);
-        arr = new Character[H][W];
-
-        for (int i = 0; i < H; i++) {
-            char[] input = br.readLine().toCharArray();
-            for (int j = 0; j < W; j++) {
-                arr[i][j] = input[j];
-            }
-        }
-
-        boolean[] hasBeen = new boolean[26];
-        hasBeen[arr[0][0] - 'A'] = true;
-        dfs(hasBeen, 0, 0, 1);
-
-        System.out.print(maxDistance);
-    }
-
-    public static void dfs(boolean[] hasBeen, int x, int y, int curDistance) {
-        maxDistance = Math.max(curDistance, maxDistance);
-        
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (isInRange(nx, ny) && !hasBeen[arr[nx][ny] - 'A']) {
-                hasBeen[arr[nx][ny] - 'A'] = true;
-                dfs(hasBeen, nx, ny, curDistance + 1);
-                hasBeen[arr[nx][ny] - 'A'] = false;
-            }
-        }
-
-    }
-
-    private static boolean isInRange(int nx, int ny) {
-        return 0 <= nx && nx < H && 0 <= ny && ny < W;
-    }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] HW = br.readLine().split(" ");
+		H = Integer.parseInt(HW[0]);
+		W = Integer.parseInt(HW[1]);
+		board = new char[H][W];
+		for (int x = 0; x < H; x++) {
+			String input = br.readLine();
+			for (int y = 0; y < W; y++) {
+				board[x][y] = input.charAt(y);
+			}
+		}
+		
+		boolean[] passedAlphabet = new boolean['Z' - 'A' + 1];
+		passedAlphabet[board[0][0] - 'A'] = true;
+		dfs(0, 0, 1, passedAlphabet);
+		
+		System.out.print(maxMoves);
+	}
+	
+	public static int maxMoves = Integer.MIN_VALUE;
+	public static char[][] board;
+	public static int H;
+	public static int W;
+	
+	public static int[] dx = new int[]{0, 0, 1, -1};
+	public static int[] dy = new int[]{1, -1, 0, 0};
+	
+	public static void dfs(int x, int y, int moves, boolean[] passedAlphabet) {
+		maxMoves = Math.max(maxMoves, moves);
+		
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (0 <= nx && nx < H && 0 <= ny && ny < W && !passedAlphabet[board[nx][ny] - 'A']) {
+				passedAlphabet[board[nx][ny] - 'A'] = true;
+				dfs(nx, ny, moves + 1, passedAlphabet);
+				passedAlphabet[board[nx][ny] - 'A'] = false;
+			}
+		}
+	}
 }
