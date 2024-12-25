b7aaceabec28a3e8dbc601d2da1898f0912dbd0b
이창근 <dlckdrms0517@gmail.com>
2024-12-25 17:35:34 +0900

    [Silver II] Title: 주식, Time: 1060 ms, Memory: 381440 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -1,37 +1,50 @@
-import java.util.*;
 import java.io.*;
-import java.util.stream.Collectors;
+import java.util.StringJoiner;
 
 class Main {
-    
-    private String solution(int arrSize, List<Integer> list) {
-        int currentMax = 0;
-        
-        long result = 0;
-        for (int i = list.size() - 1; i >= 0; i--) {
-            int current = list.get(i);
-            
-            if (current > currentMax) {
-                currentMax = current;
-            } else {
-                result += currentMax - current;
-            }
-        }
-        
-        return result + "\n";
-    }
-    
+	
+	private static int DAY;
+	private static int[] STOCKS;
+	
 	public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testcase = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < testcase; i++) {
-            int arrSize = Integer.parseInt(br.readLine());
-            String[] arr = br.readLine().split(" ");
-            List<Integer> list = Arrays.stream(arr).map(Integer::parseInt).collect(Collectors.toList());
-            sb.append(new Main().solution(arrSize, list));
-        }
-        System.out.print(sb);
-        br.close();
-    }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringJoiner sj = new StringJoiner("\n");
+		int testcase = Integer.parseInt(br.readLine());
+		
+		while (testcase-- > 0) {
+			// 데이터 입력
+			DAY = Integer.parseInt(br.readLine());
+			STOCKS = new int[DAY];
+			String[] stocks = br.readLine().split(" ");
+			for (int i = 0; i < DAY; i++) {
+				STOCKS[i] = Integer.parseInt(stocks[i]);
+			}
+			
+			// 계산 및 결과 추가
+			long result = solution();
+			sj.add(String.valueOf(result));
+		}
+		
+		// 결과 출력
+		System.out.print(sj);
+	}
+	
+	/*
+	전체 최대 이익은 각 날짜 최대 이익의 합이다.
+	각 날짜 최대 이익은 각 날짜 이후 날짜 중 가장 주식이 높은 날짜에서 판 이익이다.
+	 */
+	private static long solution() {
+		long result = 0;
+		
+		int maxStock = STOCKS[DAY - 1];
+		for (int i = DAY - 2; i >= 0; i--) {
+			if (maxStock < STOCKS[i]) {
+				maxStock = STOCKS[i];
+			} else {
+				result += (maxStock - STOCKS[i]);
+			}
+		}
+		
+		return result;
+	}
 }
