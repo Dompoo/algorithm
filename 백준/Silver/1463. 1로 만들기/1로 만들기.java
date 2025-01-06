d7836eca09d74b0700b92407563750e21afe9b42
이창근 <dlckdrms0517@gmail.com>
2025-01-06 15:08:41 +0900

    [Silver III] Title: 1로 만들기, Time: 132 ms, Memory: 18120 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,33 @@
+/*
+X가 3으로 나누어 떨어지면, 3으로 나눈다.
+X가 2로 나누어 떨어지면, 2로 나눈다.
+1을 뺀다.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int number = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[number + 1];
+		Arrays.fill(dp, Integer.MAX_VALUE);
+		dp[number] = 0;
+		
+		for (int i = number; i >= 1; i--) {
+			if (i % 3 == 0) {
+				dp[i / 3] = Math.min(dp[i / 3], dp[i] + 1);
+			}
+			if (i % 2 == 0) {
+				dp[i / 2] = Math.min(dp[i / 2], dp[i] + 1);
+			}
+			dp[i - 1] = Math.min(dp[i - 1], dp[i] + 1);
+		}
+		
+		System.out.print(dp[1]);
+	}
+}
