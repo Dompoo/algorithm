9523b00382ad774eee8fd410119193fe4d0d3516
이창근 <dlckdrms0517@gmail.com>
2025-08-26 22:59:45 +0900

    [Silver IV] Title: 큐 2, Time: 1432 ms, Memory: 494864 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+
+        int count = Integer.parseInt(br.readLine());
+
+        String[] commands = new String[count];
+        for (int i = 0; i < count; i++) {
+            commands[i] = br.readLine();
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+
+        for (int i = 0; i < count; i++) {
+            String command = commands[i];
+            if (command.startsWith("push")) {
+                int number = Integer.parseInt(command.split(" ")[1]);
+                deque.offer(number);
+            } else if (command.equals("pop")) {
+                Integer result = deque.poll();
+                sb.append(result == null ? -1 : result).append("\n");
+            } else if (command.equals("size")) {
+                sb.append(deque.size()).append("\n");
+            } else if (command.equals("empty")) {
+                sb.append(deque.isEmpty() ? 1 : 0).append("\n");
+            } else if (command.equals("front")) {
+                Integer result = deque.peekFirst();
+                sb.append(result == null ? -1 : result).append("\n");
+            } else if (command.equals("back")) {
+                Integer result = deque.peekLast();
+                sb.append(result == null ? -1 : result).append("\n");
+            } else {
+                throw new IllegalStateException();
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
