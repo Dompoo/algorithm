018666e7a7acfb9ee25905707d15a6040a35b381
이창근 <dlckdrms0517@gmail.com>
2025-08-31 15:21:21 +0900

    [Silver II] Title: A → B, Time: 108 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -1,51 +1,37 @@
-import java.io.*;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
-
-    static class Number {
-        public final long value;
-        public final long attempt;
-
-        public Number(final long value, final long attempt) {
-            this.value = value;
-            this.attempt = attempt;
-        }
-
-        public Number withDouble() {
-            return new Number(value * 2, attempt + 1);
-        }
-
-        public Number withAddOneLeft() {
-            return new Number(value * 10 + 1, attempt + 1);
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        final var br = new BufferedReader(new InputStreamReader(System.in));
-        final var AB = br.readLine().split(" ");
-        long A = Integer.parseInt(AB[0]);
-        long B = Integer.parseInt(AB[1]);
-        
-        Deque<Number> deque = new ArrayDeque<>();
-        deque.offer(new Number(A, 1));
-        long result = -1;
-        while (!deque.isEmpty()) {
-            final var number = deque.poll();
-
-            if (number.value == B) {
-                result = number.attempt;
+/*
+1은 X2로 만들지 못한다!
+ */
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int result = 1;
+        while (true) {
+            if (A > B) {
+                System.out.println(-1);
                 break;
-            }
-
-            if (number.value * 2 <= B) {
-                deque.offer(number.withDouble());
-            }
-            if (number.value * 10 + 1 <= B) {
-                deque.offer(number.withAddOneLeft());
+            } else if (A == B) {
+                System.out.println(result);
+                break;
+            } else {
+                if (B % 2 == 0) {
+                    B /= 2;
+                    result++;
+                } else if (B % 10 == 1) {
+                    B -= 1;
+                    B /= 10;
+                    result++;
+                } else {
+                    System.out.println(-1);
+                    break;
+                }
             }
         }
-        System.out.println(result);
     }
 }
