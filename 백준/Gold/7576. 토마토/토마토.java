fbc6869f421626f974d81f7619a01ea7b80ff50d
이창근 <dlckdrms0517@gmail.com>
2025-09-16 15:31:30 +0900

    [Gold V] Title: 토마토, Time: 592 ms, Memory: 104428 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,99 +1,101 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-class Main {
-	
-	private static int X;
-	private static int Y;
-	
-	private static final int[] dx = new int[]{0, 0, 1, -1};
-	private static final int[] dy = new int[]{1, -1, 0, 0};
-	
-	private static int[][] box;
-	
-	private static int addedTomatoCount = 0;
-	
-	public static class Tomato {
-		
-		public int ripenDay;
-		public int x;
-		public int y;
-		
-		public Tomato(int ripenDay, int x, int y) {
-			this.ripenDay = ripenDay;
-			this.x = x;
-			this.y = y;
-		}
-		
-		public List<Tomato> getNextTomatos() {
-			List<Tomato> nextTomatos = new ArrayList<>();
-			for (int i = 0; i < 4; i++) {
-				int nx = x + dx[i];
-				int ny = y + dy[i];
-				if (0 <= nx && nx < X && 0 <= ny && ny < Y && box[nx][ny] == 0) {
-					nextTomatos.add(new Tomato(ripenDay + 1, nx, ny));
-					box[nx][ny] = 1;
-					addedTomatoCount++;
-				}
-			}
-			return nextTomatos;
-		}
-		
-		@Override
-		public boolean equals(Object o) {
-			if (this == o) return true;
-			if (!(o instanceof Tomato)) return false;
-			Tomato tomato = (Tomato) o;
-			return ripenDay == tomato.ripenDay && x == tomato.x && y == tomato.y;
-		}
-		
-		@Override
-		public int hashCode() {
-			return Objects.hash(ripenDay, x, y);
-		}
-	}
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String[] XY = br.readLine().split(" ");
-		X = Integer.parseInt(XY[1]);
-		Y = Integer.parseInt(XY[0]);
-		
-		box = new int[X][Y];
-		
-		int tomatoCountToAdd = 0;
-		Queue<Tomato> ripenTomatos = new LinkedList<>();
-		for (int i = 0; i < X; i++) {
-			String[] inputs = br.readLine().split(" ");
-			for (int j = 0; j < Y; j++) {
-				int input = Integer.parseInt(inputs[j]);
-				box[i][j] = input;
-				if (input == 1) {
-					ripenTomatos.offer(new Tomato(0, i, j));
-				}
-				if (input == 0) {
-					tomatoCountToAdd++;
-				}
-			}
-		}
-		
-		int day = 0;
-		while(!ripenTomatos.isEmpty()) {
-			Tomato currentTomato = ripenTomatos.poll();
-			List<Tomato> nextTomatos = currentTomato.getNextTomatos();
-			for (Tomato nextTomato : nextTomatos) {
-				day = Math.max(nextTomato.ripenDay, day);
-				ripenTomatos.offer(nextTomato);
-			}
-		}
-		
-		if (addedTomatoCount == tomatoCountToAdd) {
-			System.out.print(day);
-		} else {
-			System.out.print(-1);
-		}
-	}
+@SuppressWarnings("ALL")
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int Y = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[X][Y];
+        boolean[][] added = new boolean[X][Y];
+        Deque<Tomato> deque = new ArrayDeque<>();
+        int tomatoToAdd = 0;
+        for (int i = 0; i < X; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < Y; j++) {
+                int input = Integer.parseInt(st.nextToken());
+                if (input == 1) {
+                    added[i][j] = true;
+                    deque.add(new Tomato(i, j, 0));
+                }
+                if (input == 0) {
+                    tomatoToAdd++;
+                }
+                arr[i][j] = input;
+            }
+        }
+
+        int[] dx = new int[]{0, 0, 1, -1};
+        int[] dy = new int[]{1, -1, 0, 0};
+        int totalAddedCount = 0;
+        int lastDate = 0;
+        while (!deque.isEmpty()) {
+            Tomato tomato = deque.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = tomato.x + dx[i];
+                int ny = tomato.y + dy[i];
+                if (nx < 0 || nx >= X || ny < 0 || ny >= Y || arr[nx][ny] == -1 || added[nx][ny] == true) continue;
+                added[nx][ny] = true;
+                deque.add(new Tomato(nx, ny, tomato.date + 1));
+                lastDate = tomato.date + 1;
+                totalAddedCount++;
+            }
+        }
+
+        if (tomatoToAdd == totalAddedCount) {
+            System.out.println(lastDate);
+        } else {
+            System.out.println(-1);
+        }
+    }
+
+    private static void printTomatos(Set<Tomato> tomatos, int X, int Y) {
+        String[][] arr = new String[X][Y];
+        for (String[] strings : arr) {
+            Arrays.fill(strings, "X");
+        }
+        for (Tomato tomato : tomatos) {
+            arr[tomato.x][tomato.y] = "O";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String[] strings : arr) {
+            for (String string : strings) {
+                sb.append(string);
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static class Tomato {
+
+        int x;
+        int y;
+        int date;
+
+        Tomato(int x, int y, int date) {
+            this.x = x;
+            this.y = y;
+            this.date = date;
+        }
+
+        @Override
+        public final boolean equals(Object o) {
+            if (!(o instanceof Tomato)) return false;
+
+            Tomato tomato = (Tomato) o;
+            return x == tomato.x && y == tomato.y;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = x;
+            result = 31 * result + y;
+            return result;
+        }
+    }
 }
